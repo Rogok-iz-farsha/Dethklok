b2c97630ae42b9585aa63c79e8a5e4cb0503bf33
Rogok-iz-farsha <luchiks94@mail.ru>
2014-02-19 09:42:51 +0400

    cos&&scalar

---
+++ Vector/src/VecGo.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * @author Alex_Rozhkov
+ */
+public class VecGo {
+    public static void main(String[] args) {
+    System.out.println ("Ready");
+    double l=5;
+    
+    Vector v1=new Vector (1,2,3), v2=new Vector (0,-4,9); 
+    System.out.println ("(" + v1 + ") + (" + v2 + ") = " + v1.sum (v2));
+    System.out.println ("(" + v1 + ") - (" + v2 + ") = " + v1.subtruct (v2));
+    System.out.println ("(" + v1 + ") * ("+l+") = " + v1.multiply_const(l));
+    System.out.println ("[v1,v2] = " + v1.multiply_vec(v2));
+    //System.out.println ("(v1,v2) = " + scalar(v1,v2));    Ошибки
+    //System.out.println ("Cos(v1,v2) = " + cos(v1,v2));   
+    
+    }
+    
+}
