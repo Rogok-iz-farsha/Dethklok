1fe0a373e81bb37c2bfaaea1b1eb7ef4943c4182
Rogok-iz-farsha <luchiks94@mail.ru>
2014-03-18 10:19:31 +0400

    Line

--- seminars/class06/src/main/java/ru/mipt/spring2014/class06/ConsoleDemo.java
+++ seminars/class06/src/main/java/ru/mipt/spring2014/class06/ConsoleDemo.java
@@ -1,6 +1,5 @@
 /**
- * ConsoleDemo.java
- * Created on Mar 12, 2014
+ * ConsoleDemo.java Created on Mar 12, 2014
  */
 package ru.mipt.spring2014.class06;
 
@@ -8,12 +7,31 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class ConsoleDemo
-{
-	public static void main (String[] args) throws IOException
-	{
-		final BufferedReader stdIn = new BufferedReader (new InputStreamReader (System.in));
-		String line = stdIn.readLine ();
-		System.out.println (">>>> " + line);
-	}
+public class ConsoleDemo {
+
+    public static void main(String[] args) throws IOException {
+        final BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        for (;;) {
+            String line = stdIn.readLine();
+            char c = 0;
+            if (line.length() != 0) {
+                c = line.toCharArray()[line.length() - 1];
+            }
+
+            if (c == '/') {
+                do {
+                    line = line.concat("\n");
+                    String newLine = stdIn.readLine();
+                    c = line.toCharArray()[newLine.length() - 1];
+                    line = line.concat(newLine);
+                } while (c == '/');
+            }
+            if ("exit".equalsIgnoreCase(line)) {
+                break;
+            } else {
+                System.out.println(">>>> " + line);
+            }
+
+        }
+    }
 }
