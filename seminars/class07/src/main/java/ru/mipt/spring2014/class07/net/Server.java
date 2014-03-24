7e7253c4d7fa9d5242ddae42980a5eaa8ffed27c
Rogok-iz-farsha <luchiks94@mail.ru>
2014-03-24 21:04:10 +0400

    Thread_pool

--- seminars/class07/src/main/java/ru/mipt/spring2014/class07/net/Server.java
+++ seminars/class07/src/main/java/ru/mipt/spring2014/class07/net/Server.java
@@ -6,8 +6,12 @@ package ru.mipt.spring2014.class07.net;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
-public class Server implements Runnable
+
+public class Server implements Runnable 
 {
 	private final int port;
 
@@ -16,24 +20,25 @@ public class Server implements Runnable
 		this.port = port;
 	}
 
-	public void run ()
+	public void run() 
 	{
 		final ServerSocket socket;
+		final ExecutorService execSrv = Executors.newFixedThreadPool (1);
+		final int tasksNumber = 4;
+		final Future<Runnable>[] tasks = new Future[tasksNumber];
 		try{
 			System.out.println ("Server: Start accepting connections");
 			socket = new ServerSocket (port);
-
-			try{
-				while (!Thread.interrupted ())
-				{
-					new Thread (new RequestHandler (socket.accept ())).start ();
-					System.out.println ("Server: Connection accepted");
-				}
-			} finally
-			{
-				System.out.println ("Server: Stop accepting requests");
+         
+			for(int i=0; i<tasksNumber; i++)
+		{
+			tasks[i] = (Future<Runnable>) execSrv.submit (new RequestHandler (socket.accept ()));
+			System.out.println ("Server: Connection accepted");
+		}
+			
+                System.out.println ("Server: Stop accepting requests");
 				socket.close ();
-			}
+			
 		} catch (IOException ex)
 		{
 			System.err.println ("Server: Exception in server thread: " + ex);
